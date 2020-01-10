121a0b64789d28fab0384cef73d40510a8de441c
lisheng <892958173@qq.com>
2020-01-11 00:24:17 +0800

    finished：三路快速排序 next：进一步消化三种快排

--- QuickSort/src/QuickSort2.java
+++ QuickSort/src/QuickSort2.java
@@ -4,8 +4,7 @@ import java.util.Arrays;
  * @author LiSheng
  * @date 2020/1/10 20:20
  */
-public class QuickSort {
-
+public class QuickSort2 {
     public static void sort(Comparable[] arr) {
         int n = arr.length;
         //对arr[l...r]部分进行快速排序
@@ -17,51 +16,55 @@ public class QuickSort {
             InsertionSort.insertionSort(arr, l, r);
             return;
         }
-        int p = partition2(arr, l, r);
-        quickSort(arr, l, p - 1);
-        quickSort(arr, p + 1, r);
+        int[] res = quick3Way(arr, l, r);
+        quickSort(arr, l, res[0] - 1);
+        quickSort(arr, res[1], r);
     }
 
-    // 对arr[l...r]部分进行partition操作
-    // 返回p，使得arr[l...p-1] < arr[p]; arr[p+1...r]>arr[p]
-    private static int partition(Comparable[] arr, int l, int r) {
+    /**
+     * 三路快速排序处理 arr[l...r]
+     * 将arr[l...r]分为 <v ； =v; >v;
+     *
+     * @param arr
+     * @param l
+     * @param r
+     * @return
+     */
+    public static int[] quick3Way(Comparable[] arr, int l, int r) {
         swap(arr, l, (int) (Math.random() * (r - l) + l));
         Comparable v = arr[l];
-        int j = l;
-        // arr[l+1...j]<v ; arr[j+1...i)>v
-        for (int i = l + 1; i <= r; i++) {
+        // arr[l+1...lt]<v ; arr[lt+1...i-1]=v ; arr[gt...r]>0
+        int lt = l, gt = r + 1;
+        for (int i = l + 1; i < gt; i++) {
             if (arr[i].compareTo(v) < 0) {
-                swap(arr, ++j, i);
+                swap(arr, lt + 1, i);
+                lt++;
+//                i++;
+            } else if (arr[i].compareTo(v) == 0) {
+//                i++;
+            } else {
+                swap(arr, --gt, i);
+                i--;
             }
         }
-        swap(arr, l, j);
-        return j;
-    }
-
-    private static int partition2(Comparable[] arr, int l, int r) {
-        swap(arr, l, (int) (Math.random() * (r - l) + l));
-        Comparable v = arr[l];
-        // arr[l+1...i)<=v ; arr(j...r]>=v
-        int i = l + 1, j = r;
-        while (true) {
-            while (i <= r && arr[i].compareTo(v) < 0) {
+        /*int i = l + 1;
+        while (i < gt) {
+            if (arr[i].compareTo(v) < 0) {
+                swap(arr, lt + 1, i);
+                lt++;
                 i++;
+            } else if (arr[i].compareTo(v) == 0) {
+                i++;
+            } else {
+                swap(arr, --gt, i);
             }
-            while (j >= l + 1 && arr[j].compareTo(v) > 0) {
-                j--;
-            }
-            if (i > j) {
-                break;
-            }
-            swap(arr, i, j);
-            i++;
-            j--;
-        }
-        swap(arr, l, j);
-        return j;
+        }*/
+        swap(arr, l, lt);
+//        quick3Way(arr, l, lt - 1);
+//        quick3Way(arr, gt, r);
+        int[] res = {lt, gt};
+        return res;
     }
-
-
 
     private static void swap(Object[] arr, int j, int i) {
         Object tmp = arr[j];
@@ -73,18 +76,18 @@ public class QuickSort {
         int n = 1000000;
         Integer[] arr = SortTestHelper.generateRandomArray(n, 0, n);
         Integer[] arr2 = Arrays.copyOfRange(arr, 0, arr.length);
-        SortTestHelper.testSort("QuickSort", arr);
+        SortTestHelper.testSort("QuickSort2", arr);
         SortTestHelper.testSort("MergeSort", arr2);
 
         //测试基本有序的数组
         Integer[] arr3 = SortTestHelper.generateNearlyOrderedArray(n, 100);
         Integer[] arr4 = Arrays.copyOfRange(arr3, 0, arr3.length);
-        SortTestHelper.testSort("QuickSort", arr3);
+        SortTestHelper.testSort("QuickSort2", arr3);
         SortTestHelper.testSort("MergeSort", arr4);
 
         Integer[] arr5 = SortTestHelper.generateRandomArray(n, 0, 10);
         Integer[] arr6 = Arrays.copyOfRange(arr5, 0, arr5.length);
-        SortTestHelper.testSort("QuickSort", arr5);
+        SortTestHelper.testSort("QuickSort2", arr5);
         SortTestHelper.testSort("MergeSort", arr6);
 
 
